0324de43eeecc86f04b9dbeed327dca4781d48cd
DarrellWulff <WereWulffStudios@gmail.com>
2020-03-11 23:46:02 -0700

    Ready to fill in Async tasks and need to flesh out postRenderer and postmaker, merge to develop soon

---
+++ app/src/main/java/com/github/P4rzival/RadiusMessage/PostMaker.java
@@ -0,0 +1,14 @@
+package com.github.P4rzival.RadiusMessage;
+
+import android.app.Application;
+
+import org.json.JSONObject;
+
+public class PostMaker extends Application {
+
+    //Async Task
+    public void createPost(JSONObject newPostJSON)
+    {
+
+    }
+}
