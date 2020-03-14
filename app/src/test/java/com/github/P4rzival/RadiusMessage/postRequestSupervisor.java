e3898cd805d2ce3e86247cbccd500e5aa6f52799
Matthew Sullivan <Matthew.Sullivan971@myci.csuci.edu>
2020-03-14 13:50:11 -0700

    Added a constructor and a class member for keeping track of spinlocking to the postRequestSupervisor class

--- app/src/test/java/com/github/P4rzival/RadiusMessage/postRequestSupervisor.java
+++ app/src/test/java/com/github/P4rzival/RadiusMessage/postRequestSupervisor.java
@@ -3,6 +3,13 @@ package com.github.P4rzival.RadiusMessage;
 import org.json.*;
 
 public class postRequestSupervisor {
+
+    boolean shouldContinue = false;
+
+    public postRequestSupervisor() {
+        shouldContinue = false;
+    }
+
     void PRS(JSONObject newPost) {
         while (PRSContinue() != true) ;
 
