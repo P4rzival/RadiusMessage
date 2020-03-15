9d339dec8964713e263e05a0a42ba730abddbbee
Matthew Sullivan <Matthew.Sullivan971@myci.csuci.edu>
2020-03-15 15:17:52 -0700

    added a skeleton for an AsyncTask for the DatabaseAccessor

--- app/src/test/java/com/github/P4rzival/RadiusMessage/DatabaseAccessor.java
+++ app/src/test/java/com/github/P4rzival/RadiusMessage/DatabaseAccessor.java
@@ -1,5 +1,7 @@
 package com.github.P4rzival.RadiusMessage;
 
+import android.os.AsyncTask;
+
 import org.json.JSONObject;
 
 public class DatabaseAccessor {
@@ -12,6 +14,31 @@ public class DatabaseAccessor {
 
     public static void databaseRequestApproval(PostRequestSupervisor postRequestSupervisor) {
         databaseFakeRequestApproval(postRequestSupervisor);
+    }
+
+
+    private class databaseRequestTask extends AsyncTask<PostRequestSupervisor, Long, Integer> {
+
+        long timeoutCounter = 0l;
+        long previousTime = System.currentTimeMillis();
+        long currentTime = previousTime;
+
+        @Override
+        protected Integer doInBackground(PostRequestSupervisor... postRequestSupervisors) {
+
+            databaseFakeRequestApproval(postRequestSupervisors[0]);
+
+            return 0;
+        }
+
+        @Override
+        protected void onProgressUpdate(Long... progress) {
+            timeoutCounter += progress[0];
+        }
+
+        @Override
+        protected void onPostExecute(Integer result) {
+        }
     }
 
 }
