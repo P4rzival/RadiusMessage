d7a832f98b684d825469f37a0398e55ef872593d
Matthew Sullivan <Matthew.Sullivan971@myci.csuci.edu>
2020-03-15 14:39:07 -0700

    JSONObject is now in the format Darrell's rendering code expects

--- app/src/test/java/com/github/P4rzival/RadiusMessage/RequestGenerator.java
+++ app/src/test/java/com/github/P4rzival/RadiusMessage/RequestGenerator.java
@@ -4,13 +4,18 @@ import org.json.*;
 
 public class RequestGenerator {
 
-    public static void generateRequest(String postText, int postRadius, int postExpDelay) {
+    public static void generateRequest(String userTextMessage,
+                                       double radius,
+                                       long messageDuration) {
+
         JSONObject newPost = new JSONObject();
 
         try {
-            newPost.put("postText", postText);
-            newPost.put("postRadius", postRadius);
-            newPost.put("postExpirationDelay", postExpDelay);
+            newPost.put("userTextMessage", userTextMessage);
+            newPost.put("radius", radius);
+            newPost.put("locationX", null);
+            newPost.put("locationY", null);
+            newPost.put("messageDuration", messageDuration);
         }
         catch (JSONException e) {
             e.printStackTrace();
