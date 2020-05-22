22d0f0c96ba7fa789335bc6ab1d888dd9aaa4fbb
Matthew Sullivan <Matthew.Sullivan971@myci.csuci.edu>
2020-05-21 17:09:15 -0700

    added a method (and two helper class implementations) for requesting and then drawing posts pulled from the server

--- app/src/main/java/com/github/P4rzival/RadiusMessage/DatabaseAccessor.java
+++ app/src/main/java/com/github/P4rzival/RadiusMessage/DatabaseAccessor.java
@@ -1,10 +1,23 @@
 package com.github.P4rzival.RadiusMessage;
 
 import android.os.AsyncTask;
+import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 public class DatabaseAccessor {
+
+    public static RequestQueue requestQueue =
+            Volley.newRequestQueue(RadiusMessage.getAppInstance().getApplicationContext());
 
     public DatabaseAccessor() { }
 
@@ -15,7 +28,6 @@ public class DatabaseAccessor {
     public static void databaseRequestApproval(PostRequestSupervisor postRequestSupervisor) {
         new DatabaseRequestTask().executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, postRequestSupervisor);
     }
-
 
     private static class DatabaseRequestTask extends AsyncTask<PostRequestSupervisor, Long, Integer> {
 
@@ -38,6 +50,58 @@ public class DatabaseAccessor {
 
         @Override
         protected void onPostExecute(Integer result) {
+        }
+    }
+
+    public static void getPostsFromLocation(double latitude, double longitude) {
+        // TODO: This is hardcoded for now until we can figure out how to make it related to level
+        //  of zoom
+        double radius = 200;
+
+        String url = "https://radiusmessage.cikeys.com/posts/GetPostsFromLocation.php?lat="
+                + latitude + "&lon=" + longitude + "&rad=" + radius;
+
+        // Volley doesn't have proper documentation, so I'll rattle off the parameters here as of
+        //  20200521.
+        // method: the HTTP method to use
+        // url: URL to fetch the JSON from
+        // jsonRequest: A JSONArray to post with the request. Null is allowed and
+        //               indicates no parameters will be posted along with request.
+        // listener: Listener to receive the JSON response
+        // errorListener: Error listener, or null to ignore errors.
+        JsonArrayRequest jsonObjectRequest = new JsonArrayRequest(Request.Method.GET,
+                url, null, new GetPostsRequestListener(),
+                new GetPostsRequestErrorListener());
+
+        requestQueue.add(jsonObjectRequest);
+    }
+
+    private static class GetPostsRequestListener implements Response.Listener<JSONArray> {
+        @Override
+        public void onResponse(JSONArray response) {
+
+            PostDrawer postDrawer = new PostDrawer();
+
+            try {
+                for(int i=0; i<response.length(); i++) {
+                    postDrawer.createPost(response.getJSONObject(i));
+                }
+            } catch(JSONException je) {
+                System.out.println("Error in GetPostsRequestListener.onResponse: "
+                + je.getMessage());
+            }
+        }
+    }
+
+    private static class GetPostsRequestErrorListener implements Response.ErrorListener {
+        @Override
+        public void onErrorResponse(VolleyError error) {
+
+            System.out.println("Error in getPostsFromLocation: " + error.getMessage());
+
+            Toast.makeText(RadiusMessage.getAppInstance().getApplicationContext(),
+                    "Failed to get new posts. Check network connection.",
+                    Toast.LENGTH_SHORT).show();
         }
     }
 
