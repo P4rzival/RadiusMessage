b806c70870fd53de37b47f5978c2df47b32ea1bd
Matthew Sullivan <Matthew.Sullivan971@myci.csuci.edu>
2020-05-21 22:43:32 -0700

    added everything necessary for sending posts to the server

--- app/src/main/java/com/github/P4rzival/RadiusMessage/DatabaseAccessor.java
+++ app/src/main/java/com/github/P4rzival/RadiusMessage/DatabaseAccessor.java
@@ -8,12 +8,15 @@ import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.osmdroid.util.GeoPoint;
+
+import java.net.URLEncoder;
 
 public class DatabaseAccessor {
 
@@ -39,7 +42,36 @@ public class DatabaseAccessor {
         @Override
         protected Integer doInBackground(PostRequestSupervisor... postRequestSupervisors) {
 
-            databaseFakeRequestApproval(postRequestSupervisors[0]);
+            // We only have a single PostRequestSupervisor, but AsyncTask insists we have the
+            //  option for the possibility of multiple
+            PostRequestSupervisor postRequestSupervisor = postRequestSupervisors[0];
+            String url;
+
+            try {
+                url = "https://radiusmessage.cikeys.com/posts/SubmitPost.php?text=" +
+                        URLEncoder.encode(
+                                (String) postRequestSupervisor.post.get("user_message_text"))
+                        + "&rad=" + postRequestSupervisor.post.get("radius")
+                        + "&lat=" + postRequestSupervisor.post.get("latitude")
+                        + "&lon=" + postRequestSupervisor.post.get("longitude")
+                        + "&der=" + postRequestSupervisor.post.get("message_duration")
+                        + "&del=" + postRequestSupervisor.post.get("message_delay")
+                        + "&img=" + postRequestSupervisor.post.get("user_message_image");
+
+            } catch (JSONException e) {
+                System.out.println("Debug: Problem in DatabaseRequestTask: " + e.getMessage());
+                e.printStackTrace();
+
+                // TODO 20200521: Is this what we do here? Honestly, if this messes up, the app
+                //  should just quit.
+                return -1;
+            }
+
+            StringRequest stringRequest =
+                    new StringRequest(url, new DatabaseRequestListener(),
+                            new DatabaseRequestErrorListener());
+
+            requestQueue.add(stringRequest);
 
             return 0;
         }
@@ -116,4 +148,27 @@ public class DatabaseAccessor {
         getPostsFromLocation(currentLocation.getLatitude(), currentLocation.getLongitude());
     }
 
+
+    private static class DatabaseRequestListener implements Response.Listener<String> {
+        @Override
+        public void onResponse(String response) {
+            // There are no strings that give us a clue as to whether or not it worked at the
+            //  moment, so just do nothing for now
+        }
+    }
+
+    private static class DatabaseRequestErrorListener implements Response.ErrorListener {
+        @Override
+        public void onErrorResponse(VolleyError error) {
+
+            System.out.println("Error in DatabaseRequestTask: " + error.getMessage());
+
+            Toast.makeText(RadiusMessage.getAppInstance().getApplicationContext(),
+                    "Failed to make post. Check network connection.",
+                    Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
 }
+
